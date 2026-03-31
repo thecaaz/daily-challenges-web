877293d97e0903c6ce4dd311f5cb3a82145fd25a
thecaaz <o.arth@live.de>
2026-04-01 00:27:20 +0200

    chore: split contracts and implementations by directory

---
+++ backend/Repositories/Contracts/IGameRepository.cs
@@ -0,0 +1,12 @@
+using DailyChallenges.Models;
+namespace DailyChallenges.Repositories
+{
+    public interface IGameRepository
+    {
+        Task<List<Game>> GetAllAsync();
+        Task<Game?> GetByIdAsync(int id);
+        Task<Game> CreateAsync(Game game);
+        Task<Game> UpdateAsync(Game game);
+        Task DeleteAsync(int id);
+    }
+}
