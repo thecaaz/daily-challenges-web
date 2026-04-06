a5f6e4b98f4147f4cdda464a2b38b23ab466b457
thecaaz <o.arth@live.de>
2026-04-06 15:50:45 +0200

    extract nav item element

--- frontend/src/components/NavDrawer.jsx
+++ frontend/src/components/NavDrawer.jsx
@@ -1,42 +1,16 @@
 import React from 'react'
-import { Drawer, Box, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
-import { Link, useLocation } from 'react-router-dom'
+import { Drawer, Box, List } from '@mui/material'
 import HomeIcon from '@mui/icons-material/Home'
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings'
+import NavDrawerItem from './NavDrawerItem'
 
 export default function NavDrawer({ open, onClose, user }) {
-  const location = useLocation()
-
-  const isActive = (to) => {
-    if (to === '/') return location.pathname === '/'
-    return location.pathname.startsWith(to)
-  }
-
   return (
     <Drawer anchor="left" open={open} onClose={onClose}>
       <Box sx={{ width: 250 }} role="presentation" onClick={onClose} onKeyDown={onClose}>
         <List>
-          <ListItemButton
-            component={Link}
-            to="/"
-            selected={isActive('/')}
-            sx={{ '&.Mui-selected, &.Mui-selected:hover': { backgroundColor: (theme) => theme.palette.action.selected } }}
-          >
-            <ListItemIcon><HomeIcon /></ListItemIcon>
-            <ListItemText primary="Home" />
-          </ListItemButton>
-
-          {user?.isAdmin && (
-            <ListItemButton
-              component={Link}
-              to="/admin"
-              selected={isActive('/admin')}
-              sx={{ '&.Mui-selected, &.Mui-selected:hover': { backgroundColor: (theme) => theme.palette.action.selected } }}
-            >
-              <ListItemIcon><AdminPanelSettingsIcon /></ListItemIcon>
-              <ListItemText primary="Admin" />
-            </ListItemButton>
-          )}
+          <NavDrawerItem to="/" icon={<HomeIcon />} primary="Home" />
+          {user?.isAdmin && <NavDrawerItem to="/admin" icon={<AdminPanelSettingsIcon />} primary="Admin" />}
         </List>
       </Box>
     </Drawer>
