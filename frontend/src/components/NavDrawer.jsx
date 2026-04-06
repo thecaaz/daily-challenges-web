1eb53f14cc4fac9158c88efbe04f93d2e7b16f6c
thecaaz <o.arth@live.de>
2026-04-06 15:49:02 +0200

    show isactive in drawer

--- frontend/src/components/NavDrawer.jsx
+++ frontend/src/components/NavDrawer.jsx
@@ -1,20 +1,38 @@
 import React from 'react'
 import { Drawer, Box, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import HomeIcon from '@mui/icons-material/Home'
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings'
 
 export default function NavDrawer({ open, onClose, user }) {
+  const location = useLocation()
+
+  const isActive = (to) => {
+    if (to === '/') return location.pathname === '/'
+    return location.pathname.startsWith(to)
+  }
+
   return (
     <Drawer anchor="left" open={open} onClose={onClose}>
       <Box sx={{ width: 250 }} role="presentation" onClick={onClose} onKeyDown={onClose}>
         <List>
-          <ListItemButton component={Link} to="/">
+          <ListItemButton
+            component={Link}
+            to="/"
+            selected={isActive('/')}
+            sx={{ '&.Mui-selected, &.Mui-selected:hover': { backgroundColor: (theme) => theme.palette.action.selected } }}
+          >
             <ListItemIcon><HomeIcon /></ListItemIcon>
             <ListItemText primary="Home" />
           </ListItemButton>
+
           {user?.isAdmin && (
-            <ListItemButton component={Link} to="/admin">
+            <ListItemButton
+              component={Link}
+              to="/admin"
+              selected={isActive('/admin')}
+              sx={{ '&.Mui-selected, &.Mui-selected:hover': { backgroundColor: (theme) => theme.palette.action.selected } }}
+            >
               <ListItemIcon><AdminPanelSettingsIcon /></ListItemIcon>
               <ListItemText primary="Admin" />
             </ListItemButton>
