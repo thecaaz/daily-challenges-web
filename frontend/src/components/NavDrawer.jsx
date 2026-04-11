1d3fa6314526cf51579c231176f0073be6a68d33
thecaaz <o.arth@live.de>
2026-04-11 12:39:43 +0200

    feat: add Extension page and integrate into navigation

--- frontend/src/components/NavDrawer.jsx
+++ frontend/src/components/NavDrawer.jsx
@@ -3,6 +3,7 @@ import { Drawer, Box, List } from '@mui/material'
 import HomeIcon from '@mui/icons-material/Home'
 import DashboardIcon from '@mui/icons-material/Dashboard'
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings'
+import ExtensionIcon from '@mui/icons-material/Extension'
 import NavDrawerItem from './NavDrawerItem'
 
 export default function NavDrawer({ open, onClose, user }) {
@@ -13,6 +14,7 @@ export default function NavDrawer({ open, onClose, user }) {
           <NavDrawerItem to="/" icon={<HomeIcon />} primary="Home" />
           {user && <NavDrawerItem to="/dashboard" icon={<DashboardIcon />} primary="Dashboard" />}
           {user?.isAdmin && <NavDrawerItem to="/admin" icon={<AdminPanelSettingsIcon />} primary="Admin" />}
+          <NavDrawerItem to="/extension" icon={<ExtensionIcon />} primary="Extension" />
         </List>
       </Box>
     </Drawer>
